de012f7fa71a6a5da4a2ac57b093b709bea566f2
DNghia111098 <hungdai111098@gmail.com>
2023-12-04 19:20:48 +0700

    first commit

--- test-1/bai2/bai2.js
+++ test-1/bai2/bai2.js
@@ -2,7 +2,7 @@ let sothuc= [5 , 7 , 9 , -5, -8, -10]
 
 for(let i = 0; i < sothuc.leght; i++) {
     if (sothuc[i] < 0) {
-        if (sothuc[i] = n[i] * -1); 
+        sothuc[i] = sothuc[i] * -1;
     }
-}
+}     
 console.log(sothuc)
